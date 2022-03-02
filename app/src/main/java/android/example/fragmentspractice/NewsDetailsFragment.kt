fd68fecb08f087ef77c3491ec07ef9bdb745e501
John <jmitropoulos@alumni.berklee.edu>
2022-03-02 11:57:41 -0600

    Added News Details Fragment

---
+++ app/src/main/java/android/example/fragmentspractice/NewsDetailsFragment.kt
@@ -0,0 +1,20 @@
+package android.example.fragmentspractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class NewsDetailsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        var layout : View = inflater.inflate(R.layout.fragment_newsdetails,container,false)
+        return layout;
+    }
+}
