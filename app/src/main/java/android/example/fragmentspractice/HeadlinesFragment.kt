79a1d15397bbb054392ef1c3f9f6c1dfc73888e7
John <jmitropoulos@alumni.berklee.edu>
2022-03-02 11:31:56 -0600

    Initial commit

---
+++ app/src/main/java/android/example/fragmentspractice/HeadlinesFragment.kt
@@ -0,0 +1,38 @@
+package android.example.fragmentspractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+class HeadlinesFragment : Fragment(), AdapterView.OnItemClickListener {
+    lateinit var headlinesListview: ListView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        var layout: View = inflater.inflate(R.layout.fragment_headlines, container, false)
+        headlinesListview = layout.findViewById(R.id.headlinesListview)
+        headlinesListview.setOnItemClickListener(this)
+        return layout
+    }
+
+    override fun onItemClick(adapter: AdapterView<*>?, row: View?, position: Int, rowId: Long) {
+        var item  = adapter?.getItemAtPosition(position).toString()
+        //Toast.makeText(activity, item, Toast.LENGTH_SHORT).show()
+        displayToast(item)
+    }
+
+    fun Fragment.displayToast( message: String){
+        Toast.makeText(activity,"extension function --"+message,Toast.LENGTH_SHORT).show()
+    }
+
+}
